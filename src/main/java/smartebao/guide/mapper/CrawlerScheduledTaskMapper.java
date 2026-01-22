bc244b42b63f4cf21a3d8c7922ab42fab8a2eb2a
齐雨争 <qiyz@smartebao.com>
2026-01-22 13:36:45 +0800

    edit

--- src/main/java/smartebao/guide/mapper/CrawlerScheduledTaskMapper.java
+++ src/main/java/smartebao/guide/mapper/CrawlerScheduledTaskMapper.java
@@ -15,13 +15,13 @@ public interface CrawlerScheduledTaskMapper extends BaseMapper<CrawlerScheduledT
      * 查询所有启用的定时任务
      * 使用自定义查询避免MySQL保留字enabled的问题
      */
-    @Select("SELECT id, task_key, client_id, username, script_id, domain, interval, `enabled`, execute_on_idle, task_name, create_time, update_time FROM crawler_scheduled_task WHERE `enabled` = 1")
+    @Select("SELECT id, task_key, client_id, username, script_id, domain, `interval`, `enabled`, execute_on_idle, task_name, create_time, update_time FROM crawler_scheduled_task WHERE `enabled` = 1")
     List<CrawlerScheduledTask> selectAllEnabledTasks();
     
     /**
      * 根据客户端ID查询定时任务
      */
-    @Select("SELECT id, task_key, client_id, username, script_id, domain, interval, `enabled`, execute_on_idle, task_name, create_time, update_time FROM crawler_scheduled_task WHERE client_id = #{clientId}")
+    @Select("SELECT id, task_key, client_id, username, script_id, domain, `interval`, `enabled`, execute_on_idle, task_name, create_time, update_time FROM crawler_scheduled_task WHERE client_id = #{clientId}")
     List<CrawlerScheduledTask> selectByClientId(String clientId);
     
     /**
